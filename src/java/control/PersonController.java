5aeba5443dfdcb8ed009b83a3d8a530fe9ad0f5c
hardboilr <tobias.cbs@gmail.com>
2015-10-01 20:29:25 +0200

    test

--- src/java/control/PersonController.java
+++ src/java/control/PersonController.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /**
- * @author Tobias Jacobsen
+ * @author Tobias Jacobsen sd
  */
 public class PersonController implements PersonFacade {
     
